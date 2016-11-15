415ebe7e1fab94ebb0a50807e15a80e2d2e7f9d1
Denis Sablin <diverdiver@yandex.ru>
2016-11-15 17:25:08 +0300

    my first test example code added

---
+++ csharp-example/csharp-example/MyFirstTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+        [Test]
+        public void Firsttest()
+        {
+            _driver.Url = "http://www.google.com/";
+            _driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            _driver.FindElement(By.Name("btnG")).Click();
+            _wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
