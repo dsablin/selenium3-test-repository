fc16ccd3893aa2ec5d24fb397acb559ffdbdd023
Denis Sablin <diverdiver@yandex.ru>
2016-11-16 12:24:27 +0300

    Задание 3
    Логин в админку магазина (а заодно и логаут)

---
+++ csharp-example/csharp-example/LiteCartAdminLoginTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class LiteCartAdminLoginTests
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+        [Test]
+        public void LiteCartAdminLoginTest()
+        {
+            _driver.Url = "http://localhost/litecart/admin/";
+            _driver.FindElement(By.Name("username")).SendKeys("admin");
+            _driver.FindElement(By.Name("password")).SendKeys("admin");
+            _driver.FindElement(By.Name("login")).Click();
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.Id("box-apps-menu-wrapper")));
+            _driver.FindElement(By.ClassName("fa-sign-out")).Click();
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.Name("login")));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
